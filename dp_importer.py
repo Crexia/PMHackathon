b35f396574af2b25c0006add0d64e6e8e5380baa
umut.dural <umut.dural@rwth-aachen.de>
2022-04-29 19:59:25 +0200

    Created db_importer.py

---
+++ dp_importer.py
@@ -0,0 +1,10 @@
+from pycelonis import get_celonis
+
+celonis = get_celonis(
+          url= "https://academic-umut-dural-rwth-aachen-de.eu-2.celonis.cloud/",
+          api_token= "MjIyYjViMDEtMDE4Yi00YTM0LWI0OGYtZjRlNjgyNDBkZmYyOnQySkRBV0hyUmFjUllWUldBeXZHaFFzZHRPN1psL21SVWFScXh0UjdUUkRE"
+)
+
+print(celonis.datamodels)
+
+955669d9-c78c-49eb-9982-85af2a7d1e24
