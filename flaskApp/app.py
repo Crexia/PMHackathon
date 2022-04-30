5f97e487aa1770d465da8bcc2a5d7566c9af767b
Martin <mn.vichev@gmail.com>
2022-04-30 13:00:02 +0200

    Dfg creation

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -4,12 +4,15 @@ from pycelonis import get_celonis
 from pycelonis.notebooks import api_tutorial
 from pycelonis.celonis_api.pql import pql
 from pm4py.objects.log.exporter.xes import exporter as xes_exporter
+from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
 import pm4py
+import networkx as nx
 
 app = Flask(__name__)
 
 celonis = None
 df = None
+log = None
 
 @app.route("/")
 def hello_world():
@@ -26,6 +29,7 @@ def getClusters():
         return make_response({'error': 'No epsilon specified'})
     min_pts = request.form['min_pts']
 
+    global celonis
     celonis = get_celonis(
         url="https://academic-umut-dural-rwth-aachen-de.eu-2.celonis.cloud/",
         api_token="MjIyYjViMDEtMDE4Yi00YTM0LWI0OGYtZjRlNjgyNDBkZmYyOnQySkRBV0hyUmFjUllWUldBeXZHaFFzZHRPN1psL21SVWFScXh0UjdUUkRE"
@@ -40,8 +44,10 @@ def getClusters():
     q += pql.PQLColumn('"mobis_challenge_log_2019_csv"."CASE"', 'case:concept:name')
     q += pql.PQLColumn('"mobis_challenge_log_2019_csv"."ACTIVITY"', "concept:name")
     q += pql.PQLColumn('CLUSTER_VARIANTS ( VARIANT("mobis_challenge_log_2019_csv"."ACTIVITY"),' + min_pts + ', ' + epsilon +')', 'Cluster')
+    global df
     df = mobis._get_data_frame(q)
 
+    global log
     log = pm4py.convert_to_event_log(df)
 
     # xes_exporter.apply(log, 'logs.xes')
@@ -54,6 +60,22 @@ def getClusters():
 
 @app.route('/get_dfg/', methods=['POST', 'GET'])
 def getDFG():
+    # log = pm4py.convert_to_event_log(df)
+    global log
+    print("Log NNone: " + str(log is None))
     dfg = dfg_discovery.apply(log)
-    print(dfg)
-    return make_response({'dfg': dfg})
+
+    ngraph = nx.DiGraph()
+    ngraph.add_edges_from(dfg)
+    # print("Is gra")
+    json_ngraph = nx.node_link_data(ngraph)
+    # print(json_ngraph)
+    print(ngraph.nodes)
+    print(ngraph.edges)
+    return make_response({'dfg_nodes': ngraph.nodes,
+                          'dfg_edges': ngraph.edges})
+
+@app.route('/get_activity_freq/', methods=['POST', 'GET'])
+def getFreq():
+    pass
+    # return make_response({'result': dfg})
